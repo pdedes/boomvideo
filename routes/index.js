571d8b95021fa7004736e7e00f55eef08d95c5c3
pdedes <dedes1821@gmail.com>
2015-05-16 12:10:21 -0400

    cleaned up socket.io installation.

--- routes/index.js
+++ routes/index.js
@@ -30,6 +30,7 @@ module.exports = function(io) {
 
 	/* GET home page. */
 	router.get('/*', function(req, res, next) {
+	  io.emit('route test', { number: 5 });	
 	  console.log('/* route');
 	  res.render('index', { title: 'BoomVideo', invites: [1, 2, 3, 4] });
 	});
